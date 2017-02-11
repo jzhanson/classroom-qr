d60fab4fecd4bad834292b8342bfa889475ec225
Jennifer Lee <jennifer.lee.ny@gmail.com>
2017-02-10 22:38:57 -0500

    editedStudent.swift

--- Classroom QR/Student.swift
+++ Classroom QR/Student.swift
@@ -9,12 +9,48 @@
 import UIKit
 import Foundation
 
+extension String {
+    
+    var length: Int {
+        return self.characters.count
+    }
+    
+    subscript (i: Int) -> String {
+        return self[Range(i ..< i + 1)]
+    }
+    
+    func substring(from: Int) -> String {
+        return self[Range(min(from, length) ..< length)]
+    }
+    
+    func substring(to: Int) -> String {
+        return self[Range(0 ..< max(0, to))]
+    }
+    
+    subscript (r: Range<Int>) -> String {
+        let range = Range(uncheckedBounds: (lower: max(0, min(length, r.lowerBound)),
+                                            upper: min(length, max(0, r.upperBound))))
+        let start = index(startIndex, offsetBy: range.lowerBound)
+        let end = index(start, offsetBy: range.upperBound - range.lowerBound)
+        return self[Range(start ..< end)]
+    }
+    
+}
+
 class Student {
     
     struct Student {
         var name = ""
-        var history = []
+        var history = [1 : "A"]
     }
     
+    func addScore (input : String) {
+        if input.isEmpty {
+            print("QR value returns empty string")
+        }
+        else {
+            
+        }
+    }
     
 }
