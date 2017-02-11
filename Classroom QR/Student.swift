62a633c0e6100bd7254118bc25ea1e39d335238f
Jennifer Lee <jennifer.lee.ny@gmail.com>
2017-02-10 22:27:22 -0500

    added Student.swift

---
+++ Classroom QR/Student.swift
@@ -0,0 +1,20 @@
+//
+//  studentObject.swift
+//  Classroom QR
+//
+//  Created by Jennifer Lee on 2/10/17.
+//  Copyright © 2017 Josh Zhanson. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class Student {
+    
+    struct Student {
+        var name = ""
+        var history = []
+    }
+    
+    
+}
