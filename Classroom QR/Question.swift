b67a35854ff94b5ce04573609ef470beafd674c0
Jennifer Lee <jennifer.lee.ny@gmail.com>
2017-02-11 13:51:20 -0500

    FINISHED SWEEP :)

--- Classroom QR/Question.swift
+++ Classroom QR/Question.swift
@@ -11,8 +11,9 @@ import UIKit
 
 class Question {
     
-    var question = ""
+    var question = 0
     var solution = ""
+    var students = [Student]()
     var studentAnswers = [String : String]()
 
     init (inquiry : String) {
@@ -23,21 +24,32 @@ class Question {
         solution = sol
     }
     
-    func addStudentAnswers (child : String, answer : String) {
-        studentAnswers[child] = answer
+    func addStudentToQuestion(child : String) {
+        studentAnswers[child] = "-"
     }
     
-    func sweep(sweepResult : Set<String>) {
-        let bob = Student(firstName: "Bob")
-        let fatigue = Student(firstName: "La Fatigue.")
-        let testtwo = Student(firstName : "Test T")
-        let test = Student(firstName: "Test")
+    func addStudentAnswer (child : String, answer : String) {
+        studentAnswers[child] = answer
+    }
+    //sweep takes in output of QR code scanning and list of student objects...
+    func sweep(sweepResult : Set<String> ) {
+        let Bob = Student(firstName: "Bob")
+        let Fatigue = Student(firstName: "La Fatigue.")
+        let Testtwo = Student(firstName : "Test T")
+        let Test = Student(firstName: "Test")
     
-        let listOfStudents = [bob, fatigue, testtwo, test]
-        for student in listOfStudents {
-            student.addStudentAnswer(questionNum : 1, sweepSet : sweepResult)//persistData.input)
-            print(student.name)
-            print(student.history)
+        let students = [Bob, Fatigue, Testtwo, Test]
+        //student is string and answer is string
+        for student in students {
+            for str in sweepResult {
+                let name = str[0..<str.length-2]
+                let ans = str[str.length-1]
+                
+                if (student.name == name) {
+                    studentAnswers[student.name] = ans
+                    student.history[question] = ans
+                }
+            }
         }
     }
 }
